99b4a1de163adaecf5a7ad6d9916044e0569a09d
astakhov <astakhov@iep.ru>
2024-12-31 20:27:27 +0300

    DZ5 methods

--- src/js/app.js
+++ src/js/app.js
@@ -1,24 +1,5 @@
-import Character from './Character';
+// TODO: write your code here
+import sum from './basic';
 
-function createCharacter(name, type) {
-  try {
-    const character = new Character(name, type);
-    console.log(`Создан персонаж: ${character.name}, Тип: ${character.type}, Уровень: ${character.level}, Здоровье: ${character.health}, Атака: ${character.attack}, Защита: ${character.defence}`);
-    return character;
-  } catch (error) {
-    console.error('Ошибка создания персонажа:', error.message);
-  }
-}
-
-
-const archer = createCharacter('Archer', 'Bowman');
-
-
-if (archer) {
-
-  archer.levelUp();
-  console.log(`После повышения уровня: Уровень: ${archer.level}, Здоровье: ${archer.health}, Атака: ${archer.attack}, Защита: ${archer.defence}`);
-  
-  archer.damage(10);
-  console.log(`После урона: Здоровье: ${archer.health}`);
-}
+console.log('worked');
+console.log(sum([1, 2]));
