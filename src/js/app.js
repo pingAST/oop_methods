849ecf105c0cf10214a2286b99e7ef4f9554678b
astakhov <astakhov@iep.ru>
2024-12-31 20:31:13 +0300

    DZ5 methods

--- src/js/app.js
+++ src/js/app.js
@@ -1,5 +1,22 @@
 // TODO: write your code here
+import Character from './Character';
 import sum from './basic';
 
 console.log('worked');
 console.log(sum([1, 2]));
+
+try {
+  const archer = new Character('Archer', 'Bowman');
+
+  console.log(`Создан персонаж: ${archer.name}, Тип: ${archer.type}, Уровень: ${archer.level}, Здоровье: ${archer.health}, Атака: ${archer.attack}, Защита: ${archer.defence}`);
+
+  // Пример повышения уровня
+  archer.levelUp();
+  console.log(`После повышения уровня: Уровень: ${archer.level}, Здоровье: ${archer.health}, Атака: ${archer.attack}, Защита: ${archer.defence}`);
+
+  // Пример урона
+  archer.damage(10);
+  console.log(`После урона: Здоровье: ${archer.health}`);
+} catch (error) {
+  console.error('Ошибка:', error.message);
+}
