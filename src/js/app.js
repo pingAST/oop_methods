b1010ea64e6ed65a21803c3de91154ae400be5fd
astakhov <astakhov@iep.ru>
2024-12-31 19:48:29 +0300

    DZ5 methods

--- src/js/app.js
+++ src/js/app.js
@@ -1,9 +1,5 @@
 // TODO: write your code here
 import Character from './Character';
-import sum from './basic';
-
-console.log('worked');
-console.log(sum([1, 2]));
 
 const archer = new Character('Archer', 'Bowman');
 console.log(`Создан персонаж: ${archer.name}, Тип: ${archer.type}, Уровень: ${archer.level}, Здоровье: ${archer.health}, Атака: ${archer.attack}, Защита: ${archer.defence}`);
