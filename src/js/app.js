e81204d974516b7073ed50733193cb2b11f253e1
astakhov <astakhov@iep.ru>
2024-12-31 19:55:51 +0300

    DZ5 methods

--- src/js/app.js
+++ src/js/app.js
@@ -1,13 +1,24 @@
-// TODO: write your code here
 import Character from './Character';
 
-const archer = new Character('Archer', 'Bowman');
-console.log(`Создан персонаж: ${archer.name}, Тип: ${archer.type}, Уровень: ${archer.level}, Здоровье: ${archer.health}, Атака: ${archer.attack}, Защита: ${archer.defence}`);
+function createCharacter(name, type) {
+  try {
+    const character = new Character(name, type);
+    console.log(`Создан персонаж: ${character.name}, Тип: ${character.type}, Уровень: ${character.level}, Здоровье: ${character.health}, Атака: ${character.attack}, Защита: ${character.defence}`);
+    return character;
+  } catch (error) {
+    console.error('Ошибка создания персонажа:', error.message);
+  }
+}
 
-// Пример повышения уровня
-archer.levelUp();
-console.log(`После повышения уровня: Уровень: ${archer.level}, Здоровье: ${archer.health}, Атака: ${archer.attack}, Защита: ${archer.defence}`);
 
-// Пример урона
-archer.damage(10);
-console.log(`После урона: Здоровье: ${archer.health}`);
+const archer = createCharacter('Archer', 'Bowman');
+
+
+if (archer) {
+
+  archer.levelUp();
+  console.log(`После повышения уровня: Уровень: ${archer.level}, Здоровье: ${archer.health}, Атака: ${archer.attack}, Защита: ${archer.defence}`);
+  
+  archer.damage(10);
+  console.log(`После урона: Здоровье: ${archer.health}`);
+}
