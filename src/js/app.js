093b927578880651522493396a371eebc1ceb4a7
astakhov <astakhov@iep.ru>
2025-01-01 15:11:08 +0300

    DZ

--- src/js/app.js
+++ src/js/app.js
@@ -1,22 +1,9 @@
 // TODO: write your code here
-import Bowerman from "./characters/Bowerman";
+import Character from './characters/Character';
 import sum from './basic';
 
 console.log('worked');
 console.log(sum([1, 2]));
 
-const hero = new Bowerman('Hero', 'Bowman');
+const hero = new Character('Hero', 'Bowman');
 console.log(hero);
-
-// метод levelUp
-console.log('Уровень до повышения:', hero.level);
-hero.levelUp();
-console.log('Уровень после повышения:', hero.level);
-console.log('Здоровье после повышения уровня:', hero.health);
-console.log('Атака после повышения уровня:', hero.attack);
-console.log('Защита после повышения уровня:', hero.defence);
-
-// метод damage
-console.log('Здоровье до получения урона:', hero.health);
-hero.damage(10);
-console.log('Здоровье после получения урона:', hero.health);
